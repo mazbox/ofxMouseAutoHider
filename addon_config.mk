0669b2b10d82613ff7eb0aeb9a92616701f9e740
Memo Akten <memo@memo.tv>
2019-02-01 04:50:01 +0300

    addon_config

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+meta:
+	ADDON_NAME = ofxMouseAutoHider
+	ADDON_DESCRIPTION = Auto hide mouse
+	ADDON_AUTHOR = Memo Akten, www.memo.tv
+	ADDON_TAGS = ""
+	ADDON_URL = https://github.com/memo/ofxMouseAutoHider
+
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	ADDON_DEPENDENCIES = 
